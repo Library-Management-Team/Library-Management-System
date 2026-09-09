6371ea657462ecb8d8c8ddb709cbf625e955c5af
lana <lanaabusafat2006@gmail.com>
2026-09-10 01:42:31 +0300

    Add membership tier to member

--- SystemCode/Member.java
+++ SystemCode/Member.java
@@ -6,12 +6,13 @@ public class Member {
     private String id;
     private String name;
     private String contactInfo;
+    private MembershipTier membershipTier;
 
-    public Member(String name, String contactInfo) {
+    public Member(String name, String contactInfo, MembershipTier membershipTier) {
         id = String.valueOf(nextId++);
-        ;
         this.name = name;
         this.contactInfo = contactInfo;
+        this.membershipTier = membershipTier;
     }
 
     public String getId() {
@@ -24,6 +25,10 @@ public class Member {
 
     public String getContactInfo() {
         return contactInfo;
+    }
+
+    public MembershipTier getTier(){
+        return membershipTier;
     }
 
 }
