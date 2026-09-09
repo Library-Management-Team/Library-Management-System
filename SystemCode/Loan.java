1b4b07da2127e6646d6acedfc94526f9c504dd98
lana <lanaabusafat2006@gmail.com>
2026-09-09 23:42:19 +0300

    Rename BorrowDetails to Loan

---
+++ SystemCode/Loan.java
@@ -0,0 +1,35 @@
+package SystemCode;
+
+import java.time.LocalDate;
+
+public class Loan {
+
+    private Member member;
+    private Copy copy;
+    private LocalDate borrowDate;
+    private LocalDate dueDate;
+
+    public Loan(Member member, Copy copy) {
+        this.member = member;
+        this.copy = copy;
+        this.borrowDate = LocalDate.now();
+        this.dueDate = borrowDate.plusDays(copy.getItem().getLoanPeriodDays());
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Copy getCopy() {
+        return copy;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+}
