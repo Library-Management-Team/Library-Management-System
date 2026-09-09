f4a3b9d6b184472b06105e496898560eb0b83112
lana <lanaabusafat2006@gmail.com>
2026-09-10 01:33:42 +0300

    Add MembershipTier with item limit and loan period

---
+++ SystemCode/MembershipLimits.java
@@ -0,0 +1,13 @@
+package SystemCode;
+
+public final class MembershipLimits {
+
+    public static final int STANDARD_ITEM_LIMIT = 5;
+    public static final int PREMIUM_ITEMP_LIMIT = 10;
+    public static final int STANDARD_LOAN_PERIOD = 14;
+    public static final int PREMIUM_LOAN_PERIOD = 21;
+
+    private MembershipLimits() {
+
+    }
+}
