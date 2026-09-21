4a7993c51b971c85ddac3dbafffb214e3a91234d
lana <lanaabusafat2006@gmail.com>
2026-09-21 17:44:07 +0300

    Add fine limit

--- SystemCode/MembershipLimits.java
+++ SystemCode/MembershipLimits.java
@@ -6,6 +6,7 @@ public final class MembershipLimits {
     public static final int PREMIUM_ITEMP_LIMIT = 10;
     public static final int STANDARD_LOAN_PERIOD = 14;
     public static final int PREMIUM_LOAN_PERIOD = 21;
+    public static final double MAX_OUTSTANDING_FINE = 10.0;
 
     private MembershipLimits() {
 
